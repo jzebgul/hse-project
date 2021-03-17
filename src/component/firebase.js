02bcca2106dab6b7b5e0f248b7d0029c4bd78b93
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2021-03-17 11:45:37 +0500

    Updating env

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -2,13 +2,14 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_KE,
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
-console.log(config)
+
+console.log(process.env);
 const firebase = app.initializeApp(config)
-export default firebase;    
+export default firebase;
